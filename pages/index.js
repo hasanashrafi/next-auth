c0d4363e2b873c75108050a09f0139ca7b9c8bd6
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-02 19:58:17 +0330

    edit styles

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,21 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 `}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h2>Next Auth</h2>
+      <div className="z-10 max-w-5xl w-full items-center  font-mono text-sm ">
+        <h2 className="border-b-2">Next Auth</h2>
+       
         <button className="p-2 rounded bg-blue-600 m-1 text-white">
           <Link href="/signup">SignUp</Link>
         </button>
-        <button  
-        className="p-2 rounded bg-blue-600 m-1 text-white">
-                   <Link href="/signin">SignIn</Link>
-
+        <button
+          className="p-2 rounded bg-blue-600 m-1 text-white">
+          <Link href="/signin">SignIn</Link>
         </button>
+         <button
+          className="p-2 rounded bg-blue-600 m-1 text-white">
+          <Link href="/dashboard">Dashboard</Link>
+        </button>
+     
       </div>
     </main>
   );
