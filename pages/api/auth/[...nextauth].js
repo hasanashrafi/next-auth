14e765d90a16d5f750c13ff7f2a6fb63ae8b01c3
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-02 19:59:03 +0330

    edit providers options

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,27 +1,33 @@
+import User from "@/models/User";
+import { verifyPassword } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
 const authOptions = {
     session: { strategy: "jwt" },
     providers: [CredentialsProvider({
-        name: "credentials",
-        credentials: {
-            email: {
-                label: "Email",
-                type: "text",
-                placeholder: "Enter Your Email"
-            },
-            Password: {
-                label: "Password",
-                type: "Password",
-                placeholder: "Enter Your Password"
-            },
-        },
         async authorize(credentials, req) {
-            return ({ name: "hasan" })
-        }
-    })
-    ]
+            const { email, password } = credentials
+
+            try {
+                await connectDB()
+            } catch (error) {
+                throw new Error("Error in connecting to DB")
+            }
+
+            if (!email, !password) throw new Error("Invalid data")
+
+            const user = await User.findOne({ email: email })
+            if (!user) throw new Error("user doesn't exist")
+
+            const isValid = await verifyPassword(password, user.password)
+            if (!isValid) throw new Error("email or password incorrect")
+
+            return { email }
+        },
+    }),
+    ],
 }
 
 
