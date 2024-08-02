92f9b1a69e1d6f75d498a7205db87a4db232fe30
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-02 19:57:53 +0330

    create dashboard route

---
+++ pages/dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function dashboard() {
+  return (
+    <div className='min-h-screen p-5'>
+<h2 className="border-b-2 w-fit border-b-black ">
+
+    dashboard
+</h2>
+
+    </div>
+  )
+}
+
+export default dashboard
