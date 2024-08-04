b1d9740ee3fe78bb3271862d93ed09981897abbf
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-04 10:39:13 +0330

    complete logout function

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,47 @@
-import { signIn } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 
 export default function Home() {
+  const { status } = useSession()
+  console.log({ status });
+
+  const logOutHandler = () => {
+    signOut()
+  }
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+      className={`flex min-h-screen flex-col items-center justify-between p-10 `}
     >
-      <div className="z-10 max-w-5xl w-full items-center  font-mono text-sm ">
-        <h2 className="border-b-2">Next Auth</h2>
-       
-        <button className="p-2 rounded bg-blue-600 m-1 text-white">
-          <Link href="/signup">SignUp</Link>
-        </button>
-        <button
-          className="p-2 rounded bg-blue-600 m-1 text-white">
-          <Link href="/signin">SignIn</Link>
-        </button>
-         <button
-          className="p-2 rounded bg-blue-600 m-1 text-white">
-          <Link href="/dashboard">Dashboard</Link>
-        </button>
-     
+      <div className="flex justify-between z-10 max-w-5xl w-full items-center  font-mono text-sm ">
+        <h2 className="border-b-2 p-2 my-5">Next Auth</h2>
+        {
+          status === "authenticated" ? (
+            <div>
+              <button
+                className="p-2 rounded bg-green-600 m-1 text-white">
+                <Link href="/dashboard">Dashboard</Link>
+              </button>
+              <button onClick={logOutHandler}
+                className="p-2 rounded bg-red-600 m-1 text-white">
+                LogOut
+              </button>
+            </div>
+          ) : null
+        }
+        {
+          status === "unauthenticated" ? (
+            <div>
+              <button className="p-2 rounded bg-blue-600 m-1 text-white">
+                <Link href="/signup">SignUp</Link>
+              </button>
+              <button
+                className="p-2 rounded bg-blue-600 m-1 text-white">
+                <Link href="/signin">SignIn</Link>
+              </button>
+            </div>
+          ) : null
+        }
       </div>
     </main>
   );
